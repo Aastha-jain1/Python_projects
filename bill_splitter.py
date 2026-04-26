a109fc2ce6b051ed5ddc955824fbf9bc9c2c15d1
Aastha-jain1 <jaasthajain06@gmail.com>
2026-04-26 23:14:47 +0530

    Add files via upload

---
+++ bill_splitter.py
@@ -0,0 +1,6 @@
+# Bill splitter
+
+total bill = float(input("Enter total bill :"))
+people = int(input("Enter total number of people :"))
+split bill = total bill/people
+print("Amount per person :" , total bill)
---
+++ temperature_converter.py
@@ -0,0 +1,7 @@
+# Temperature convertor
+
+Celcious = float(input("Enter tempreature in celcious "))
+Feheranhite =(Celcious * 9/5) + 32
+Kelvin = (Celcious + 273.15)
+print("Temperature in feheranhite :" , Feheranhite)
+print("Temperature in kelvin :" , Kelvin)
