292069e23c95894820e8ead03560635460936760
Aastha-jain1 <jaasthajain06@gmail.com>
2026-04-27 22:46:08 +0530

    Add files via upload

---
+++ korean_grading system.py
@@ -0,0 +1,17 @@
+#Korean grading system
+
+marks = int(input("Enter your marks :"))
+if(marks>=90):
+    print("Your grade is A")
+elif(marks>=80):
+    print("Your grade is B")
+elif(marks>=70):
+    print("Your grade is C")
+elif(marks>=60):
+    print("Your grade is D")
+elif(marks>=50):
+    print("Your grade is E")
+elif(marks>=40):
+    print("Your grade id F")
+else:
+    print("YOU HAVE FAILED THIS EXAM")
